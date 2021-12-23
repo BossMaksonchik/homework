674c590b99a51ecfeafe10c97c0aef0ade640b05
Max <max9m9666@bk.ru>
2021-12-23 15:37:07 +0300

    один коммит

---
+++ src/ru/skyeng/Main.java
@@ -0,0 +1,35 @@
+package ru.skyeng;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte cucumber = 5;
+    System.out.println(cucumber);
+    short tables = 10;
+    System.out.println(tables);
+    int ball = 100;
+        System.out.println(ball);
+        long pen = 10001;
+        System.out.println(pen);
+
+        float gram = 1;
+        System.out.println(gram);
+        double water = 0.9098;
+        System.out.println(water);
+
+
+       double boxer1Weight = 78.2;
+       double boxer2Weight = 82.7;
+       double totalWeight = boxer1Weight + boxer2Weight;
+       System.out.print(" Общий вес боксеров " + totalWeight + " кг! ");
+
+       double differenceWeight = boxer2Weight - boxer1Weight;
+       System.out.print(" Разница в весе между двумя боксерами "+ differenceWeight + "кг!");
+        }
+
+
+
+
+
+    }
+
